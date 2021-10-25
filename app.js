5050f5f102a90aaea2800ca7a558fd0d0c6e9444
Bennet <bexoda@hotmail.com>
2021-10-25 12:49:48 +0000

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
