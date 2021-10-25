b2df58a548e6b7f577c430f65141495eeb8bc3b1
niicode <niidarku66@gmail.com>
2021-10-25 12:35:11 +0000

    added findOne to query

--- index.js
+++ index.js
@@ -1,53 +1,68 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const app = express();
-require('dotenv').config()
-const PORT = process.env.PORT
-const MONGO_URI = process.env.MONGO_URI
+require('dotenv').config();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose.Promise = global.Promise;
 mongoose
-    .connect(MONGO_URI, {useNewUrlParser: true})
-    .then(()=>console.log("Mongo connected successfully"))
-    .catch(err=> console.log("err"))
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('Mongo connected successfully'))
+  .catch((err) => console.log('err'));
 
-const { Article } = require('./models/Article')
+const { Article } = require('./models/Article');
 
-app.get('/', (req,res) => {
-  res.send("Hello world")
-})
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
 app.post('/new_blog', (req, res) => {
-  const article = new Article(req.body)
-  article.save((err, doc)=> {
+  const article = new Article(req.body);
+  article.save((err, doc) => {
     if (err) {
       res.json({
         success: false,
-        err
-      })
+        err,
+      });
     }
     res.status(200).json({
       success: true,
-      docs:doc
-    })
-  })
-})
+      docs: doc,
+    });
+  });
+});
 
-app.get('/blog_post', (req,res)=> {
-  Article.find({}, (err,blogs) => {
-    if(err) {
+app.get('/blog_post', (req, res) => {
+  Article.find({}, (err, blogs) => {
+    if (err) {
       return res.status.json({
-        error: err
-      })
+        error: err,
+      });
     }
-    return res.status(200).json(blogs)
-  })
-})
+    return res.status(200).json(blogs);
+  });
+});
 
-app.listen("8000", ()=> {
-  console.log(`app is listning on ${PORT}`)
-})
+app.get('/blog/:_id', (req, res) => {
+  let itemId = req.params._id;
+  Article.findOne({ _id: itemId }, (err, doc) => {
+    if (err) {
+      res.status(400).json({
+        success: false,
+        data: 'Bad request made the article does not exist.',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: doc,
+    });
+  });
+});
+
+app.listen('8000', () => {
+  console.log(`app is listning on ${PORT}`);
+});
