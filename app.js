0523277f25a1eac9118e74a2eddfb16565e35421
Bennet <bexoda@hotmail.com>
2021-10-25 13:47:45 +0000

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 // require('dotenv').config();
 const PORT = process.env.PORT;
@@ -11,22 +11,22 @@ app.use(express.json());
 mongoose.Promise = global.Promise;
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true })
-  .then(() => console.log('Mongo connected successfully'))
-  .catch((err) => console.log('err'));
+  .then(() => console.log("Mongo connected successfully"))
+  .catch((err) => console.log("err"));
 
-const { Article } = require('./models/Article');
+const { Article } = require("./models/Article");
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.post('/new_blog', (req, res) => {
+app.post("/new_blog", (req, res) => {
   const article = new Article(req.body);
   article.save((err, doc) => {
     if (err) {
       res.json({
         success: false,
-        err,
+        err:err,
       });
     }
     res.status(200).json({
@@ -36,7 +36,7 @@ app.post('/new_blog', (req, res) => {
   });
 });
 
-app.get('/blog_post', (req, res) => {
+app.get("/blog_post", (req, res) => {
   Article.find({}, (err, blogs) => {
     if (err) {
       return res.status.json({
@@ -47,13 +47,13 @@ app.get('/blog_post', (req, res) => {
   });
 });
 
-app.get('/blog/:_id', (req, res) => {
+app.get("/blog/:_id", (req, res) => {
   let itemId = req.params._id;
   Article.findOne({ _id: itemId }, (err, doc) => {
     if (err) {
       res.status(400).json({
         success: false,
-        data: 'Bad request made the article does not exist.',
+        data: "Bad request made the article does not exist.",
       });
     }
     res.status(200).json({
